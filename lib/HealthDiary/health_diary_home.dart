ac0d7eb358174b830ff786256f00029f89a05196
Josh Culbert <jculbert03@qub.ac.uk>
2025-07-25 22:21:36 +0100

    health diary in working order, minor bugs need addressed

--- lib/HealthDiary/health_diary_home.dart
+++ lib/HealthDiary/health_diary_home.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:health_app/HealthDiary/symptom_selection.dart';
 
 class HealthDiaryPage extends StatefulWidget {
   final List<SymptomEntry> healthReport;
   final List<Symptom> symptoms;
   final Function(SymptomEntry) onSave;
 
-
   const HealthDiaryPage({
     super.key,
     required this.healthReport,
     required this.symptoms,
-    required this.onSave
+    required this.onSave,
   });
 
   @override
@@ -18,49 +18,81 @@ class HealthDiaryPage extends StatefulWidget {
 }
 
 class _HealthDiaryPageState extends State<HealthDiaryPage> {
-
   final _formkey = GlobalKey<FormState>();
   final Map<String, double> ratings = {};
-  final Set<String>ignoredSymptoms = {};
+  final Set<String> ignoredSymptoms = {};
   final TextEditingController _notesController = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    for(final symptom in widget.symptoms){
-      ratings[symptom.name] = 0;
+    for (final symptom in widget.symptoms) {
+      ratings[symptom.name] = 5;
     }
   }
 
-  void toggleIgnore(String name, bool? value){
+  void toggleIgnore(String name, bool? value) {
     setState(() {
-      if(value == true){
+      if (value == true) {
         ignoredSymptoms.add(name);
-      }else{
+      } else {
         ignoredSymptoms.remove(name);
       }
     });
   }
 
-  void saveEntry(){
-    final Map<String, int>finalRatings = {};
+  void saveEntry() {
+    final Map<String, int> finalRatings = {};
 
-    for(final name in ratings.keys){
-      if(!ignoredSymptoms.contains(name)){
+    for (final name in ratings.keys) {
+      if (!ignoredSymptoms.contains(name)) {
         finalRatings[name] = ratings[name]!.round();
       }
     }
 
-    final entry = SymptomEntry(timeStamp: DateTime.now(), 
-    symptomRatings: finalRatings,
-    notes: _notesController.text.isNotEmpty ? _notesController.text:null
+    final entry = SymptomEntry(
+      timeStamp: DateTime.now(),
+      symptomRatings: finalRatings,
+      notes: _notesController.text.isNotEmpty ? _notesController.text : null,
     );
 
     widget.onSave(entry);
     Navigator.pop(context);
-
   }
 
+  Future<void> goToSymptomSelector() async {
+    final result = await Navigator.push(
+      context, 
+      MaterialPageRoute(builder: (context) => SymptomSelectionPage(
+        predefinedSymptoms: 
+      [
+        Symptom(name: "Pain"),
+        Symptom(name: "Fatigue"),
+        Symptom(name: "Sleep Quality"),
+        Symptom(name: "Mood"),
+        Symptom(name: "Nausea"),
+        Symptom(name: "Appetite"),
+        Symptom(name: "Level of activeness"),
+        Symptom(name: "Concentration"),
+        Symptom(name: "Memory"),
+      ]),
+      ));
+      if(result != null && mounted){
+        final updatedSymptomNames = result as List<String>;
+        setState(() {
+          widget.symptoms 
+          ..clear()
+          ..addAll(updatedSymptomNames.map((name)=> Symptom(name: name)));
+
+          ratings.clear();
+          for (final name in updatedSymptomNames) {
+      ratings.putIfAbsent(name, () => 5.0); // defaults to 5
+    }
+          ratings.removeWhere((key, _) => !updatedSymptomNames.contains(key));
+    ignoredSymptoms.removeWhere((name) => !updatedSymptomNames.contains(name));
+        });
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,66 +102,83 @@ class _HealthDiaryPageState extends State<HealthDiaryPage> {
     );
   }
 
-Widget _buildUI(){
-  final hasSymptoms = widget.symptoms.isNotEmpty;
+  Widget _buildUI() {
+    final hasSymptoms = widget.symptoms.isNotEmpty;
 
-  return Scaffold(
-      body: hasSymptoms ? Form(
-        key: _formkey,
-        child: ListView(
-          padding: const EdgeInsets.all(16),
-          children: [
-            ...widget.symptoms.map((sym){
-              final name = sym.name;
-              final ignored = ignoredSymptoms.contains(name);
-
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      body: hasSymptoms
+          ? Form(
+              key: _formkey,
+              child: ListView(
+                padding: const EdgeInsets.all(16),
                 children: [
-                  Text(
-                    name,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  Row(
-                    children: [
-                      Checkbox(value: ignored, onChanged: (value)=>toggleIgnore(name, value)),
-                      const Text("Ignore This Time")
-                    ],
-                  ),
-                  Slider(value: ratings[name]!,
-                  min: 1,
-                  max: 10,
-                  divisions: 9,
-                  label: ratings[name]!.round().toString(),
-                  onChanged: ignored ? null : (value)=>setState(()=>ratings[name]=value),
-                  ),
-                  const SizedBox(height: 12,)
-                ],
-              );
-            }).toList(),
-            const SizedBox(height: 20,),
-            const Text("Notes (Optional) : "),
-            TextField(
-              controller: _notesController,
-              maxLines: 3,
-              decoration: const InputDecoration(
-                hintText: "Any additional comments...",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(onPressed: saveEntry, child: const Text("Save Entry"))
-          ],
-        ))
-        : Center(
-          child: Column(
-            
-          ),
-        )
-      );
-    
+                  ElevatedButton.icon(
+                  icon: const Icon(Icons.edit),
+                  label: const Text("Manage Symptoms"),
+                  onPressed: goToSymptomSelector,
+                ),
+                const SizedBox(height: 12),
+                  ...widget.symptoms.map((sym) {
+                    final name = sym.name;
+                    final ignored = ignoredSymptoms.contains(name);
 
-}
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: ignored,
+                              onChanged: (value) => toggleIgnore(name, value),
+                            ),
+                            const Text("Ignore This Time"),
+                          ],
+                        ),
+                        Slider(
+                          value: ratings[name]!,
+                          min: 1,
+                          max: 10,
+                          divisions: 9,
+                          label: ratings[name]!.round().toString(),
+                          onChanged: ignored
+                              ? null
+                              : (value) =>
+                                    setState(() => ratings[name] = value),
+                        ),
+                        const SizedBox(height: 12),
+                      ],
+                    );
+                  }).toList(),
+                  const SizedBox(height: 20),
+                  const Text("Notes (Optional) : "),
+                  TextField(
+                    controller: _notesController,
+                    maxLines: 3,
+                    decoration: const InputDecoration(
+                      hintText: "Any additional comments...",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: saveEntry,
+                    child: const Text("Save Entry"),
+                  ),
+                ],
+              ),
+            )
+          : Center(child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("No tracked symptoms yet"),
+            const SizedBox(height: 20,),
+            ElevatedButton.icon(icon: const Icon(Icons.add), onPressed: goToSymptomSelector, label: const Text("Select Symptoms"))
+          ],)),
+    );
+  }
 }
 // Widget _SymptomEntries(){
 
@@ -143,7 +192,7 @@ class SymptomEntry {
   SymptomEntry({
     required this.timeStamp,
     required this.symptomRatings,
-    this.notes
+    this.notes,
   });
 }
 
@@ -152,9 +201,5 @@ class Symptom {
   final String? category;
   final String? desc;
 
-  Symptom({
-    required this.name,
-    this.category,
-    this.desc
-  });
+  Symptom({required this.name, this.category, this.desc});
 }
--- lib/HealthDiary/symptom_selection.dart
+++ lib/HealthDiary/symptom_selection.dart
@@ -17,7 +17,10 @@ class SymptomSelectionPage extends StatefulWidget {
 
 class _SymptomSelectionPageState extends State<SymptomSelectionPage> {
   List<String> selectedSymptomNames = [];
+  List<String> customSymptoms = [];
   final TextEditingController _symptomNameController = TextEditingController();
+
+  late SharedPreferences prefs;
 
   @override
   void initState(){
@@ -26,38 +29,68 @@ class _SymptomSelectionPageState extends State<SymptomSelectionPage> {
   }
 
   Future<void> loadStoredSymptoms() async {
-    final prefs = await SharedPreferences.getInstance();
+    prefs = await SharedPreferences.getInstance();
     setState(() {
       selectedSymptomNames = prefs.getStringList('trackedSymptoms')?? [];
+      customSymptoms = prefs.getStringList('customSymptoms') ?? [];
     });
   }
 
   Future<void> saveSymptoms() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('trackedSymptoms', selectedSymptomNames);
-    Navigator.pop(context, selectedSymptomNames);
-  }
+  customSymptoms.removeWhere((sym) => !selectedSymptomNames.contains(sym));
+
+  await prefs.setStringList('trackedSymptoms', selectedSymptomNames);
+  await prefs.setStringList('customSymptoms', customSymptoms);
+
+  Navigator.pop(context, selectedSymptomNames);
+}
+
 
   void addCustomSymptom(){
     String name = _symptomNameController.text.trim();
-    if(name.isNotEmpty && !selectedSymptomNames.contains(name)){
-      setState(() {
-        selectedSymptomNames.add(name);
-      });
-      _symptomNameController.clear();
+    String lowerName = name.toLowerCase();
+    if(name.isEmpty) return;
+
+    bool alreadyExists = [
+      ...widget.predefinedSymptoms.map((sym)=>sym.name),
+      ...customSymptoms
+    ].any((sym)=>sym.toLowerCase()==lowerName);
+    
+    if(alreadyExists){
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Symptom '$name' already tracked"))
+      );
+      return;
     }
+    final capitalised = capitalise(name);
+
+    setState(() {
+      customSymptoms.add(capitalised);
+      selectedSymptomNames.add(capitalised);
+    });
+
+    _symptomNameController.clear();
+  }
+
+  String capitalise(String text){
+    if(text.isEmpty) return text;
+    return text[0].toUpperCase() + text.substring(1).toLowerCase();
   }
 
   @override
   Widget build(BuildContext context) {
-    final allSymtoms = [...widget.predefinedSymptoms.map((sym)=> sym.name)];
+    final predefined = widget.predefinedSymptoms.map((sym)=> sym.name).toSet();
+    final allSymptoms = {
+      ...predefined,
+      ...customSymptoms
+    }.toList()..sort();
 
     return Scaffold(
       appBar: AppBar(title: const Text("Select Symptoms")),
       body: Column(
         children: [
           Expanded(child: ListView(
-            children : allSymtoms.map((name){
+            children : allSymptoms.map((name){
               return CheckboxListTile(
                 title: Text(name),
                 value: selectedSymptomNames.contains(name), 
